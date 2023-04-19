117c2934f02cdd2e0edc7c83f75b6e8b4100b354
kaoryp <58264308+kaoryp@users.noreply.github.com>
2023-04-19 10:46:46 -0300

    solution with filter method

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,10 +1,14 @@
-const removeFromArray = function(array, item) {
+/*```javascript
+removeFromArray([1, 2, 3, 4], 3); // should remove 3 and return [1,2,4]
+```*/
+//filter array method
+    const removeFromArray = function(arr, ...theArgs) {
+        
+        arr = arr.filter(arg => !theArgs.includes(arg));
+        return arr;
+    };
 
-    //filter array method
-    for(){
-
-    }
-};
+    
 
 // Do not edit below this line
 module.exports = removeFromArray;
