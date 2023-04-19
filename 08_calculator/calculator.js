69473e90e400120627fed34782e7594d13a41a92
kaoryp <58264308+kaoryp@users.noreply.github.com>
2023-04-19 14:56:28 -0300

    solution with factorial wow

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,50 @@
-const add = function() {
+const add = function(num1, num2) {
+	addition = num1 + num2;
+  return addition;
+};
+
+const subtract = function(num1, num2) {
+	subtraction = num1 - num2;
+  return subtraction;
+};
+
+const sum = function(array) {
+  total = 0;
+  for(var i in array){
+    total += array[i]
+  }
+ return total;
+};
+
+const multiply = function(array) {
+  var times = 1;
+  for(var i = 0; i < array.length; i ++){
+    times = times * array[i]
+  }
+ return times;
+
+};
+
+const power = function(num1, num2) {
+  /*
+  toThePower = num1**num2;
+  return toThePower;
+  */
+
+  //or Math.pow()
+  result = Math.pow(num1,num2);
+  return result;
 	
 };
 
-const subtract = function() {
-	
-};
-
-const sum = function() {
-	
-};
-
-const multiply = function() {
-
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+const factorial = function(number) {
+  if(number < 0){
+    return -1;
+  } else if (number == 0) {
+    return 1;
+  } else {
+    return (number * factorial(number - 1));
+  }
 	
 };
 
