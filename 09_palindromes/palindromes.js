5d185ee8802108fa43058e0ee47761b7539df107
kaoryp <58264308+kaoryp@users.noreply.github.com>
2023-04-19 15:46:20 -0300

    google def helped with this one

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,14 @@
-const palindromes = function () {
+const palindromes = function (str) {
+//all lower case and remove unwanted characters
+var re = /[\W_]/g;
 
+var lowRegStr = str.toLowerCase().replace(re, '');
+
+//chaining method from freecodecamp article
+var reverseStr = lowRegStr.split('').reverse().join(''); 
+
+//check if reverseStr equal lowRegStr
+return reverseStr === lowRegStr;
 };
 
 // Do not edit below this line
