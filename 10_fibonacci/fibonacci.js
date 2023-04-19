f1bd8903992901c52f44404342427137af8d6199
kaoryp <58264308+kaoryp@users.noreply.github.com>
2023-04-19 16:22:32 -0300

    solution

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,16 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    number = parseInt(number);
+let num1 = 0, num2 = 1, nextNum = number;
+if(number < 0){
+    return "OOPS"
+}
 
+for(let i=2; i <= number; i++){
+    nextNum = num1 + num2;
+    num1 = num2;
+    num2 = nextNum;
+   
+} return nextNum;
 };
 
 // Do not edit below this line
