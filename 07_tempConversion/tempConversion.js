a23db82661d8ec21a0560beff39e616fb69d236f
kaoryp <58264308+kaoryp@users.noreply.github.com>
2023-04-19 14:17:51 -0300

    solution formula

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let conversion = (temp - 32) * 0.5556;
+  return Math.round(conversion * 10)/10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let conversion = (temp * 1.8) + 32;
+  return Math.round(conversion * 10)/10;
 };
 
 // Do not edit below this line
